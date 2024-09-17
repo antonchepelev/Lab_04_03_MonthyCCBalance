07434dc575bf1a04ea48486e296567ab1c7c922f
antonchepelev <anton.chepelev@gmail.com>
2024-09-17 18:28:35 -0400

    task 3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double cardBalance = 5000;
+
+        double interestRate = .17;
+
+        double interestMonthOne = cardBalance * interestRate;
+
+        double interestMonthTwo = (cardBalance + interestMonthOne) * interestRate;
+
+        System.out.println("Your card balance is: " +cardBalance+
+
+                        System.lineSeparator()+
+
+                "Your interest rate is: "+interestRate+
+
+                        System.lineSeparator()+
+
+                "Your interest due after one month: "+interestMonthOne+
+
+                        System.lineSeparator()+
+
+                "Your interest due after two months: "+interestMonthTwo
+
+                );
     }
 }
